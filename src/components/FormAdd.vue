f2e16627873d95c0e55f2f5a497ee79b06cd7cc6
IvanPrz5 <maperLBR@gmail.com>
2023-01-18 18:23:12 -0600

    commit

---
+++ src/components/FormAdd.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-form ref="form" lazy-validation>
+    <v-text-field
+      label="Codigo Estado"
+      required
+    ></v-text-field>
+    <v-text-field
+      label="Nombre"
+      required
+    ></v-text-field>
+    <v-text-field
+      label="Pais"
+      required
+    ></v-text-field>
+    <v-btn color="success" class="mr-4">
+      Agregar
+    </v-btn>
+  </v-form>
+</template>
+<script>
+export default {
+  name: "FormAdd",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
---
+++ src/components/FormSat.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-container>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      sort-by="calories"
+      class="elevation-1"
+    >
+      <template v-slot:[`item.actions`]>
+        <v-icon small class="mr-2"> mdi-pencil </v-icon>
+        <v-icon small> mdi-delete </v-icon>
+      </template>
+    </v-data-table>
+  </v-container>
+</template>
+
+<script>
+//import axios from "axios";
+/* import FormAdd from "./FormAdd.vue"; */
+
+export default {
+  name: "FormSat",
+  components: {
+    /* FormAdd, */
+  },
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      {
+        text: "ID",
+        align: "start",
+        sortable: false,
+        value: "name",
+      },
+      { text: "Nombre", value: "calories", sortable: false },
+      { text: "Pais", value: "fat", sortable: false },
+      { text: "Actions", value: "actions", sortable: false },
+    ],
+  }),
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      this.desserts = [
+        {
+          name: "OAX",
+        },
+      ];
+    },
+    getDataApi() {
+      axios
+        .get("http://localhost:8081/Pais")
+        .then((response) => {
+          console.log(response.data.cpais);
+          for (let i = 0; i < response.data.length; i++) {
+            console.log(response.data[i].cpais);
+          }
+        })
+
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+  /* methods: {
+    getDataApi() {
+      axios
+        .get("http://localhost:8081/Pais")
+        .then((response) => {
+          console.log(response.data.cpais);
+          for (let i = 0; i < response.data.length; i++) {
+            console.log(response.data[i].cpais);
+          }
+        })
+
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  }, */
+};
+</script>
+
+<style></style>
